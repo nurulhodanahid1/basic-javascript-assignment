ccf9645d8e00419fede545414677401d1948e0f3
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-06-15 17:58:18 +0600

    hotel cost part completed

--- assignment.js
+++ assignment.js
@@ -1,3 +1,5 @@
+// Kilometer to Meter
+
 function kilometerToMetre(kilometer){
     if(kilometer <= 0){
         var error = "Please enter the positive value";
@@ -5,8 +7,38 @@ function kilometerToMetre(kilometer){
     }
     else{
         var metre = kilometer * 1000;
-        return metre;
     }
+    return metre;
 }
-var yourKilometer = kilometerToMetre(22);
+var yourKilometer = kilometerToMetre(-33);
 console.log(yourKilometer);
+
+
+
+// Hotel Cost
+
+function hotelCost(day){
+    if(day <= 0){
+        err = 'Please enter the positive value';
+        return err;
+    }
+    else if(day <= 10){
+        var cost = day * 100;
+    }
+    else if(day <= 20){
+        var firstPart = 10 * 100;
+        var remaining = day - 10;
+        var secondPart = remaining * 80;
+        var cost = firstPart + secondPart;
+    }
+    else{
+        var firstPart = 10 * 100;
+        var secondPart = 10 * 80;
+        var remaining = day - 20;
+        var thirdPart = remaining * 50;
+        var cost = firstPart + secondPart + thirdPart;
+    }
+    return cost;
+}
+var totalCost = hotelCost(21);
+console.log(totalCost);
